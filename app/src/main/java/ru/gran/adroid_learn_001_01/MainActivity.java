0ed1dde2e5f11030523b38589d4ac975e51fdcc9
greatkalaka <kalaka_malaka@rambler.ru>
2022-02-10 23:11:05 +0300

    Homework2

---
+++ app/src/main/java/ru/gran/adroid_learn_001_01/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.gran.adroid_learn_001_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calcular);
+    }
+}
